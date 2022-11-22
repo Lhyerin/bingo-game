962bfcba6da527a7cf61b95a87677a03e95ffc8b
Lhyerin <113421398+Lhyerin@users.noreply.github.com>
2022-11-22 18:39:48 +0900

    5번까지 함

--- bingoBoard.c
+++ bingoBoard.c
@@ -3,6 +3,8 @@
 #include <time.h>
 #include "bingoBoard.h"
 #define BINGONUM_HOLE -1
+#define BINGO_NUMSTATUS_ABSENT    -1
+#define BINGO_NUMSTATUS_PRESENT    0
 
 
 static int bingoBoard[N_SIZE][N_SIZE];
@@ -46,4 +48,59 @@ int bingo_countCompletedLine(void){
 	int i,j;
 	int cnt=0;
 	int checkBingo;
+	for(i=0; i<N_SIZE; i++){
+		checkBingo =1;
+		for (j=0;j<N_SIZE;j++){
+			if (bingoBoard[i][j]> 0){
+				checkBingo=0;
+				break;
+			}
+		}
+		if (checkBingo ==1){
+			cnt++;
+		}
+	}
+	
+	for(j=0;j<N_SIZE;j++){
+		checkBingo=1;
+		for(i=0;i<N_SIZE;i++){
+			if(bingoBoard[i][j]>0){
+			    checkBingo=0;
+			    break;
+		    }
+    	}
+		if(checkBingo==1){
+		   cnt++;
+	    }
+	}
+	for(i=0;i<N_SIZE;i++){
+		checkBingo=1;
+		if(bingoBoard[i][i]>0){
+			checkBingo=0;
+			break;
+		}
+   }
+	if(checkBingo==1){
+   	    cnt++;
+    }
+	
+	for(i=0;i<N_SIZE;i++){
+		checkBingo=1;
+		if(bingoBoard[i][N_SIZE-1-i]>0){
+			checkBingo=0;
+			break;
+		}
+   }
+	if(checkBingo==1){
+   	    cnt++;
+    }				
+   return cnt;
+}
+
+int bingo_checkNum(int selNum)
+{
+	if(numberStatus[selNum-1]==BINGONUM_HOLE)
+	  return BINGO_NUMSTATUS_ABSENT;
+	  
+	return BINGO_NUMSTATUS_PRESENT;
 }
--- main.cpp
+++ main.cpp
@@ -3,8 +3,36 @@
 #include <time.h>
 #include "bingoBoard.h"
 #define BINGONUM_HOLE -1
+#define BINGO_RES_UNFINISHED  -1
+#define BINGO_RES_FINISHED  0
+#define N_LINE 2
+
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int get_number(void)
+{
+	int selNum=0;
+	
+	do {
+		printf("select a number: ");
+		scanf("%d",&selNum);
+		fflush(stdin);
+		
+		if (selNum <1 || selNum>N_SIZE*N_SIZE || bingo_checkNum(selNum)== BINGO_NUMSTATUS_ABSENT)
+	    {
+			printf("%i is not on the board! select other one.\n",selNum);
+	    }
+	} while (selNum <1 || selNum>N_SIZE*N_SIZE || bingo_checkNum(selNum)== BINGO_NUMSTATUS_ABSENT);
+	return selNum;
+} 
+
+int check_gameEnd(void){
+    int res= BINGO_RES_UNFINISHED;
+    if(bingo_countCompletedLine()>=N_LINE)
+       res=BINGO_RES_FINISHED;
+    return res;
+}
 
 int main(int argc, char** argv) {
 	srand(unsigned(time(NULL)));
@@ -14,14 +42,24 @@ int main(int argc, char** argv) {
 	printf("=====================================================================\n\n\n\n");
 	
 	
-	//GAME
-	
-	//INITIAL BINGO BOARD
 	bingo_init();
-	bingo_inputNum(5);
-	bingo_printBoard();
-	bingo_inputNum(12);
-	bingo_printBoard();
+	
+
+	while(check_gameEnd()==BINGO_RES_UNFINISHED){
+		bingo_printBoard();
+		printf("No. of completed line :%i\n",bingo_countCompletedLine());
+        int selNum=get_number();
+        printf("%i\n",selNum);
+        bingo_inputNum(selNum);
+	}
+
+    
+		
+	
+	
+
+	
+	
 	/*
 	while (game is not end) // 홀수가 N_BINGO보다 작음
 	{
